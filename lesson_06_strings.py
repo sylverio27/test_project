d4c47d7fd2bab3e10488230e08dd080b94afbab6
Rodion <Alslaw92@gmail/com>
2026-08-26 21:02:54 +0300

    Добавил практику со строками

---
+++ lesson_06_strings.py
@@ -0,0 +1,16 @@
+name = 'Ivan'
+age = 22
+print(f"меня зовут {name}, мне {age} года")
+
+
+film = 'Tron Legacy'
+year = 2010
+print(f'{film} вышел в {year} году')
+
+
+name = input('Как тебя зовут? ')
+print(f'Привет {name}! Рад тебя видеть!')
+
+
+film = input('Какой фильм ты посмотрел? ')
+print(film.upper())
