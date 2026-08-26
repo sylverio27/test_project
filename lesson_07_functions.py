0718e626a3d05c37c209d89cc0f2ac6363ef8379
Rodion <Alslaw92@gmail/com>
2026-08-26 22:32:58 +0300

    Добавил практику по функциям

---
+++ lesson_07_functions.py
@@ -0,0 +1,23 @@
+def say_hello():
+    print('Привет, Python')
+
+say_hello()
+
+
+def greet(name):
+    print(f'Привет, {name}')
+
+greet('Anna')
+
+greet('Jora')
+
+
+def show_age(age):
+    print(f'Тебе {age} лет')
+show_age(25)
+
+
+def show_film(film, year):
+    print(f'Фильм {film} вышел в {year} году')
+
+show_film('Tron Legacy', 2010)
