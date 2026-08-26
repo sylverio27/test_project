fa51b983610bcc29812768b2ea928efe2574f20f
Rodion <Alslaw92@gmail/com>
2026-08-26 13:32:30 +0300

    Добавил практику по Python

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Hello world")
+films = ['Tron Legacy','Taxi 2', 'Taxi', 'Naruto','one piece']
+for film in films:
+    print('Я хочу посмотреть:', film)
+
+nomers = [28, 17, 11, 52, 45]
+for ci in nomers:
+    print(ci)
