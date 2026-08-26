3273b3f2d9baef993e6fc916d60997899289d17d
Rodion <Alslaw92@gmail/com>
2026-08-26 23:04:14 +0300

    Добавил практику по return

---
+++ lesson_08_return.py
@@ -0,0 +1,29 @@
+def sum_numbers(a, b):
+    return a + b
+
+result = sum_numbers(10, 5)
+print(result)
+
+
+def multiply(a, b):
+    return a * b
+
+result = multiply(10, 15)
+print(result * 2)
+
+
+def get_age_next_year(age):
+    return age + 1
+
+result = get_age_next_year(25)
+print(result)
+
+
+def is_adult(age):
+    if age >= 18:
+        return True
+    else:
+        return False
+
+result = is_adult(12)
+print(result)
